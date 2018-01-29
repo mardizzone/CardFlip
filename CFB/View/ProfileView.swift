8ef4b363507685bc91ab3b4a9f8d9b5663083e46
Michael Ardizzone <mjardizzone@gmail.com>
2018-01-28 23:24:25 -0800

    add backOfCard

---
+++ CFB/View/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  CFB
+//
+//  Created by Michael Ardizzone on 1/28/18.
+//  Copyright © 2018 Michael Ardizzone. All rights reserved.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Model.swift
@@ -0,0 +1,33 @@
+//
+//  Model.swift
+//  CFB
+//
+//  Created by Michael Ardizzone on 1/28/18.
+//  Copyright © 2018 Michael Ardizzone. All rights reserved.
+//
+
+import Foundation
+
+struct Person: Decodable {
+    let id : String
+    let name : String
+    let position : String
+    let profile_image : String
+    let personality : String
+    let interests : String
+    let dating_preferences : String
+}
+
+func loadJson(filename fileName: String) -> [Person]? {
+    if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let jsonData = try decoder.decode([Person].self, from: data)
+            return jsonData
+        } catch {
+            print("error:\(error)")
+        }
+    }
+    return nil
+}
